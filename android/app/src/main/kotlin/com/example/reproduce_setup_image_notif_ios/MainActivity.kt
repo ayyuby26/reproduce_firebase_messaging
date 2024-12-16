f649b4ed0d8acafb32678d61acb7375505cef4b1
ayub <fbayub@gmail.com>
2024-12-16 23:24:48 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/reproduce_setup_image_notif_ios/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reproduce_setup_image_notif_ios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by macbook on 15/12/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
